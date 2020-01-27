6848e9c625698d271cdcdaba91494853509fa163
alvaroximenesrj <alvaroximenesrj@gmail.com>
2020-01-27 01:14:05 -0300

    first commit

---
+++ src/Eventos.IO.Infra.CrossCutting.Identity/Authorization/JwtTokenOptions.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Threading.Tasks;
+
+namespace Eventos.IO.Infra.CrossCutting.Identity.Authorization
+{
+    public class JwtTokenOptions
+    {
+        public string Issuer { get; set; }
+
+        public string Subject { get; set; }
+        // para qual site é válido
+        public string Audience { get; set; }
+
+        public DateTime NotBefore { get; set; } = DateTime.UtcNow;
+        // quando foi emitido
+        public DateTime IssuedAt { get; set; } = DateTime.UtcNow;
+        // até quando é válido
+        public TimeSpan ValidFor { get; set; } = TimeSpan.FromHours(5);
+
+        public DateTime Expiration => IssuedAt.Add(ValidFor);
+        // 
+        public Func<Task<string>> JtiGenerator => () => Task.FromResult(Guid.NewGuid().ToString());
+        //
+        public SigningCredentials SigningCredentials { get; set; }
+    }
+}
